a78120d3284d32bc075ec07edc1907f2d5cb255b
Gustavo Schirmer <gustavo.schirmer@betrybe.com>
2021-10-14 18:35:07 -0300

    feat: configura docker login

--- build.sh
+++ build.sh
@@ -45,7 +45,7 @@ else
       --cache-from=type=registry,ref=ghcr.io/$GITHUB_REPOSITORY:$TAG \
       --cache-to=type=registry,ref=ghcr.io/$GITHUB_REPOSITORY:$CACHE_TO,mode=max"
 
-    bash -c "docker login ghcr.io -u$GITHUB_ACTOR -p$GITHUB_TOKEN"
+    bash -c "echo \"$GITHUB_TOKEN\" | docker login ghcr.io -u $GITHUB_ACTOR --password-stdin"
 
     bash -c "docker buildx create \
       --name cache-builder \
