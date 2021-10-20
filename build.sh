5c78154a523b36a8265a98edfbe912a61f319a0f
Gustavo Schirmer <gustavo.schirmer@betrybe.com>
2021-10-20 17:02:48 -0300

    feat: desfaz alteração do cache para manter compatibilidade

--- build.sh
+++ build.sh
@@ -41,8 +41,10 @@ else
     fi
 
     CACHE=" \
+      --output type=image,name=$REPO_URI:$TAG,push=true \
       --cache-from=type=registry,ref=ghcr.io/$GITHUB_REPOSITORY:production \
-      --cache-from=type=registry,ref=ghcr.io/$GITHUB_REPOSITORY:$TAG"
+      --cache-from=type=registry,ref=ghcr.io/$GITHUB_REPOSITORY:$TAG \
+      --cache-to=type=registry,ref=ghcr.io/$GITHUB_REPOSITORY:$CACHE_TO,mode=max"
 
     echo $GITHUB_TOKEN | docker login ghcr.io -u $GITHUB_ACTOR --password-stdin
 
@@ -62,10 +64,8 @@ else
   echo "::endgroup::"
 
   echo "::group::Build and push the image to ECR"
-  bash -c "docker buildx build \
-    --output type=image,name=$REPO_URI:$TAG,push=true \
+  bash -c "docker buildx build --push \
     $CACHE \
-    --cache-to=type=registry,ref=ghcr.io/$GITHUB_REPOSITORY:$CACHE_TO,mode=max \
     -f $DOCKERFILE -t $REPO_URI:$TAG $BUILD_ARGS ."
   echo "::endgroup::"
 fi
